d256d8e1c35bfd735692bfc4ccdc8231c015156c
hutson <learning2010@gmail.com>
2016-10-03 20:21:28 +0800

    explicit return

---
+++ methods/ex4.rb
@@ -0,0 +1,12 @@
+# what will the following code print to the screen?
+
+def scream(words)
+  words = words + "!!!"
+  return
+  puts words
+end
+
+
+scream("Yippeee")
+
+# It print nothing , because it uses explicit return , but nothing comes with it.
