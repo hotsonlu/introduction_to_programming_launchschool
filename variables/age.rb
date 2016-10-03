ae762327a09124ba79555cbfacc372f81172ee6b
hutson <learning2010@gmail.com>
2016-10-03 17:59:09 +0800

    Asks a user how old they are and tells them how old they will be in 10, 20, 30, 40 years.

---
+++ variables/age.rb
@@ -0,0 +1,10 @@
+puts "How old are you?"
+ages = gets.chomp.to_i
+puts "In 10 years you will be:"
+puts ages + 10
+puts "In 20 years you will be:"
+puts ages + 20
+puts "In 30 years you will be:"
+puts ages + 30
+puts "In 40 years you will be:"
+puts ages + 40
