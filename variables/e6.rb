5f4472ccd9d47e481f30576a36a33258b855e6ae
hutson <learning2010@gmail.com>
2016-10-03 18:28:57 +0800

    Undefined local variable or method error message tells what ?

---
+++ variables/e6.rb
@@ -0,0 +1,4 @@
+# NameError: undefined local variable or method `shoes' for main:Object
+
+# 1. the variable or method 'shoes' that has not been defined in the program.
+# 2. or the variable is not available to access in an outer scope.
