a51397480addbdc5b2a87b3e31052fd200a8f00a
hutson <learning2010@gmail.com>
2016-10-03 11:57:41 +0800

    movies year

---
+++ the_basics/E3.rb
@@ -0,0 +1,13 @@
+movies = { jaws: 1975,
+           the_incredibles: 2004,
+           gravity: 2013,
+           a_space_odyssey: 2001,
+           roar: 1981 }
+
+puts movies[:jaws]             
+puts movies[:the_incredibles]
+puts movies[:gravity]
+puts movies[:a_space_odyssey]
+puts movies[:roar]
+
+
