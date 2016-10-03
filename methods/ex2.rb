ca5d2b95ac150e71257f9cf9e5492f3ee3bfb02d
hutson <learning2010@gmail.com>
2016-10-03 20:11:59 +0800

    what expressions evaluate to ?

---
+++ methods/ex2.rb
@@ -0,0 +1,11 @@
+# what do the following expressions evalute to?
+
+x = 2  #  2    
+
+puts x = 2  #  nil
+
+p name = "Joe"  #  "Joe"
+
+four = "four"   # "four"
+
+print something = "nothing"  # nil
