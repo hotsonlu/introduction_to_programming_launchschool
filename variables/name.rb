b2036f4f73da540738d258a8d15f9f727fb0c02a
hutson <learning2010@gmail.com>
2016-10-03 18:12:34 +0800

    Ask the user for their first name, saves it into a variable, and then does the same for the last name, Then outputs their full name all at once.

--- variables/name.rb
+++ variables/name.rb
@@ -1,5 +1,8 @@
-puts "what's your name? " 
-name = gets.chomp
-puts "Hello #{name}, Nice to see you!"
+puts "What's your first name? " 
+first_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts first_name + " " + last_name + " Nice to see you!"
+puts "Hello #{first_name} #{last_name}, Nice to see you!"
 
-10.times {puts name}
+ 
