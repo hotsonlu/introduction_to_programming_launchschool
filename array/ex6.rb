967d18178311609bddba40da5855fb40ce1dff52
hutson <learning2010@gmail.com>
2016-10-04 22:19:05 +0800

    modify array. array is indexed with integer

---
+++ array/ex6.rb
@@ -0,0 +1,12 @@
+# you run the following code...
+
+names = ['bob', 'joe', 'susan', 'margaret']
+names['margaret'] = 'jody'
+
+# get the error message  , what's the problem, and how can it be fixed ?
+
+# Trying to modify the array ? Array is indexed with integers, not string. 
+
+names[3] = 'jody'
+
+
