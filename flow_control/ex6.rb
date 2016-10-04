f42bffcf0aa6c324af5badaef4fb068a87beb89a
hutson <learning2010@gmail.com>
2016-10-04 12:17:42 +0800

    missing 'end'

---
+++ flow_control/ex6.rb
@@ -0,0 +1,24 @@
+def equal_to_four(x)
+      if x == 4
+        puts "yup"
+      else
+        puts "nope"
+end
+
+equal_to_four(5)
+
+
+# You get the following error messge..
+# syntax error, unexpected end-of-input, expecting keyword_end
+
+# missing word 'end':  if .. else .. end 
+
+def equal_to_four(x)
+      if x == 4
+        puts "yup"
+      else
+        puts "nope"
+      end
+end
+
+equal_to_four(5)
