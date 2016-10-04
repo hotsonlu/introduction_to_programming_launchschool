4d328846b0dd12204f8c6ebdc564cdb74b8fb14a
hutson <learning2010@gmail.com>
2016-10-04 22:11:21 +0800

    array index

---
+++ array/ex4.rb
@@ -0,0 +1,8 @@
+# what does each method return in the following example?
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+arr.index(5)  # 3 ;  5 in this array, what's the index .
+
+arr.index[5]  # error
+
+arr[5]  # 8 
