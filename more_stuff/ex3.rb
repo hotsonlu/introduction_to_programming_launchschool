f26288ea4ecd6e68f6d734e9fadb66dcbc61b1b8
hutson <learning2010@gmail.com>
2016-10-05 23:32:30 +0800

    Waht's the Exception handling

---
+++ more_stuff/ex3.rb
@@ -0,0 +1,3 @@
+# What is exception handling and what problem does it solve?
+
+# Exception handling is a process that deals with errors, handling the errors in advance to avoid exiting the program.
